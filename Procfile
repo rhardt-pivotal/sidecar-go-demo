a706ecd07f956301e24a6cd1818bf438b0315428
Rob Hardt <rhardt@pivotal.io>
2016-10-06 11:48:54 -0700

    initial commit

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: server
+web: go-cloudfoundry-example
--- app.go
+++ app.go
@@ -5,11 +5,18 @@ import (
     "log"
     "net/http"
     "os"
+    "gopkg.in/resty.v0"
 )
 
 func main() {
     http.HandleFunc("/", hello)
-    err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+    http.HandleFunc("/javafortune", javafortune)
+    http.HandleFunc("/rubyfortune", rubyfortune)
+    http.HandleFunc("/pythonfortune", pythonfortune)
+    http.HandleFunc("/nodefortune", nodefortune)
+    http.HandleFunc("/dockerfortune", dockerfortune)
+    http.HandleFunc("/health", health)
+    err := http.ListenAndServe("0.0.0.0:"+os.Getenv("PORT"), nil)
     if err != nil {
         log.Fatal("ListenAndServe:", err)
     }
@@ -17,4 +24,41 @@ func main() {
 
 func hello(w http.ResponseWriter, req *http.Request) {
     fmt.Fprintln(w, "hello, world!")
+}
+
+func health(w http.ResponseWriter, req *http.Request) {
+    w.Header().Set("content-type", "application/json")
+    fmt.Fprintln(w, "{\"status\": \"UP\"}")
+}
+
+
+func fortune(w http.ResponseWriter, url string) {
+    resp, err := resty.R().Get(url)
+    if err != nil {
+        fmt.Fprintln(w, err)
+    } else {
+        w.Header().Set("content-type", "application/json")
+        fmt.Fprintln(w, resp)
+    }
+}
+
+
+func javafortune(w http.ResponseWriter, req *http.Request) {
+    fortune(w, "http://localhost:8087/fortunes/random")
+}
+
+func rubyfortune(w http.ResponseWriter, req *http.Request) {
+    fortune(w, "http://localhost:8087/ruby-demo/javafortune")
+}
+
+func pythonfortune(w http.ResponseWriter, req *http.Request) {
+    fortune(w, "http://localhost:8087/python-demo/javafortune")
+}
+
+func nodefortune(w http.ResponseWriter, req *http.Request) {
+    fortune(w, "http://localhost:8087/node-demo/javafortune")
+}
+
+func dockerfortune(w http.ResponseWriter, req *http.Request) {
+    fortune(w, "http://localhost:8087/docker-demo/javafortune")
 }
